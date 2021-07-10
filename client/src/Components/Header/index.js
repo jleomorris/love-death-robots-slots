af0a5537f867bcd0989f4fa5e8a44189921449f6
jleomorris <james-morris2010@hotmail.co.uk>
2021-07-10 21:43:15 +0100

    Add Header component

---
+++ client/src/Components/Header/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './index.scss';
+import HeaderImage from '../../Images/Header.png';
+import Slots from '../Slots';
+
+const Header = () => {
+  return (
+    <div className='header section'>
+      <h1 className='header__title'>
+        Random Episode{' '}
+        <span className='header__title header__title--highlighted'>
+          Generator
+        </span>
+      </h1>
+      <div className='img-roll-btn-container'>
+        <img className='header__image' src={HeaderImage} alt='header' />
+        {/* <div className='roll-btn'>Roll</div> */}
+      </div>
+      {/* <div className='roll-btn'>Roll</div> */}
+      <Slots />
+    </div>
+  );
+};
+
+export default Header;
