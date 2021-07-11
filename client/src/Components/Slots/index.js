ba96b235df3f93254eb1396743955238df7ec9ae
jleomorris <james-morris2010@hotmail.co.uk>
2021-07-11 04:20:50 +0100

    Add new logic, example episode data and update styling for Slots

--- client/src/Components/Slots/index.js
+++ client/src/Components/Slots/index.js
@@ -1,20 +1,51 @@
 import React, { useState, createRef } from 'react';
-import './index.css';
 import './index.scss';
 
+import Apocalypse_Tourist from '../Icons/Apocalypse_Tourist';
+import Barn from '../Icons/Barn';
+import BloodLike from '../Icons/BloodLike';
+
 const Slots = () => {
-  const fruits = ['🍒', '🍉', '🍊', '🍓', '🍇', '🥝'];
-  const [slots, setSlots] = useState({
-    fruit1: '🍒',
-    fruit2: '🍒',
-    fruit3: '🍒',
-  });
+  const episodeData = [
+    {
+      title: "Sonnie's Edge",
+      description:
+        "In the underground world of 'beastie' fights, Sonnie is unbeatable -- as long as she keeps her edge.",
+      icons: ['apocalypse_tourist', 'barn', 'blood_like'],
+    },
+    {
+      title: "Sonnie's Edge2",
+      description:
+        "In the underground world of 'beastie' fights, Sonnie is unbeatable -- as long as she keeps her edge.",
+      icons: ['barn', 'apocalypse_tourist', 'blood_like'],
+    },
+    {
+      title: "Sonnie's Edge3",
+      description:
+        "In the underground world of 'beastie' fights, Sonnie is unbeatable -- as long as she keeps her edge.",
+      icons: ['blood_like', 'barn', 'apocalypse_tourist'],
+    },
+  ];
+
+  //   const icons = ['🍒', '🍉', '🍊', '🍓', '🍇', '🥝'];
+  const icons = [
+    <Barn height={200} width={200} />,
+    <Apocalypse_Tourist height={200} width={200} />,
+    <BloodLike height={200} width={200} />,
+  ];
+  //   const [slots, setSlots] = useState({
+  //     fruit1: '🍒',
+  //     fruit2: '🍒',
+  //     fruit3: '🍒',
+  //   });
   const [isRolling, setIsRolling] = useState(false);
   const slotRefs = [createRef(), createRef(), createRef()];
 
   // to trigger rolling and maintain state
   const roll = () => {
     setIsRolling(true);
+
+    let randomEpisodeIndex = Math.floor(Math.random() * episodeData.length);
 
     setTimeout(() => {
       setIsRolling(false);
@@ -23,11 +54,13 @@ const Slots = () => {
     // Loop through slot refs and set each one to spin
     slotRefs.forEach((slot, i) => {
       // Spin current slot
-      const rolling = triggerSlotRotation(slot.current);
+      triggerSlotRotation2(slot.current, i, randomEpisodeIndex);
+      //   triggerSlotRotation(slot.current);
+      //   const updatedIcon = triggerSlotRotation(slot.current);
       // Set current slot object ref
-      const selectedFruit = `fruit${i + 1}`;
+      //   const selectedIcon = `fruit${i + 1}`;
       // Set slots with spinning slot
-      setSlots({ ...slots, [selectedFruit]: rolling });
+      //   setSlots({ ...slots, [selectedIcon]: updatedIcon });
     });
   };
 
@@ -37,10 +70,38 @@ const Slots = () => {
       ref.style.top = `${top}px`;
     };
     let options = ref.children;
-    let randomOption = Math.floor(Math.random() * fruits.length);
-    let choosenOption = options[randomOption];
+    let randomOptionIndex = Math.floor(Math.random() * icons.length);
+    let choosenOption = options[randomOptionIndex];
+    // Rotates slots
     setTop(-choosenOption.offsetTop + 2);
-    return fruits[randomOption];
+    return icons[randomOptionIndex];
+  };
+
+  const triggerSlotRotation2 = (ref, index, randomEpisodeIndex) => {
+    const setTop = (top) => {
+      ref.style.top = `${top}px`;
+    };
+
+    let options = ref.children;
+
+    let iconToChangeTo = episodeData[randomEpisodeIndex].icons[index];
+    console.log(episodeData[randomEpisodeIndex].title);
+    let chosenOptionIndex;
+
+    for (let i = 0; i < options.length; i += 1) {
+      if (
+        options[i].firstChild.firstElementChild.dataset.name === iconToChangeTo
+      ) {
+        chosenOptionIndex = i;
+      }
+    }
+
+    // debugger;
+
+    let choosenOption = options[chosenOptionIndex];
+    // Rotates slots
+    setTop(-choosenOption.offsetTop + 2);
+    // return icons[randomOptionIndex];
   };
 
   return (
@@ -48,9 +109,9 @@ const Slots = () => {
       <div className='slot'>
         <section>
           <div className='container' ref={slotRefs[0]}>
-            {fruits.map((fruit, i) => (
+            {icons.map((icon, i) => (
               <div key={i}>
-                <span>{fruit}</span>
+                <span>{icon}</span>
               </div>
             ))}
           </div>
@@ -59,9 +120,9 @@ const Slots = () => {
       <div className='slot'>
         <section>
           <div className='container' ref={slotRefs[1]}>
-            {fruits.map((fruit) => (
+            {icons.map((icon) => (
               <div>
-                <span>{fruit}</span>
+                <span>{icon}</span>
               </div>
             ))}
           </div>
@@ -70,24 +131,17 @@ const Slots = () => {
       <div className='slot'>
         <section>
           <div className='container' ref={slotRefs[2]}>
-            {fruits.map((fruit) => (
+            {icons.map((icon) => (
               <div>
-                <span>{fruit}</span>
+                <span>{icon}</span>
               </div>
             ))}
           </div>
         </section>
       </div>
-      {/* <div
-        className={!isRolling ? 'roll rolling' : 'roll'}
-        onClick={!isRolling && roll}
-        disabled={isRolling}
-      >
-        {isRolling ? 'Rolling...' : 'ROLL'}
-      </div> */}
       <div
         className={`${!isRolling ? 'roll-btn roll-btn--to-roll' : 'roll-btn'}`}
-        onClick={!isRolling && roll}
+        onClick={roll}
         disabled={isRolling}
       >
         Roll
