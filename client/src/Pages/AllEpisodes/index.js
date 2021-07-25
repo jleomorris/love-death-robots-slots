9f26b4f721ea5ba08dc4f34d635b12f34f700c6f
jleomorris <james-morris2010@hotmail.co.uk>
2021-07-25 12:34:00 +0100

    Add exit button icon to all episodes

--- client/src/Pages/AllEpisodes/index.js
+++ client/src/Pages/AllEpisodes/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import './index.scss';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 import { episodeData } from '../../episodeData';
 import DynamicEpisodeCard from '../../Components/DynamicEpisodeCard';
+import X2 from '../../Components/Icons/X2';
 
 const AllEpisodes = (props) => {
   return (
     <StyledAllEpisodes className='all-episodes'>
       <button
+        className='exit-button'
         onClick={() =>
           props.setAreAllEpisodesVisible(!props.areAllEpisodesVisible)
         }
       >
-        Close
+        <X2 />
       </button>
       {episodeData.map((episode) => (
         <DynamicEpisodeCard
@@ -33,8 +36,9 @@ const StyledAllEpisodes = styled(motion.div)`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  height: 80vh;
-  width: 80vw;
+  height: 90vh;
+  width: 90vw;
+  padding: 1rem;
   z-index: 999999;
   background: #000000d1;
   border-radius: 1rem;
